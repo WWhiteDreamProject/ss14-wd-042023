204c1504ae3a7591aaa56c18affcf13c3d99195e
rhailrake <49613070+rhailrake@users.noreply.github.com>
2023-01-18 09:31:24 +0600

    redo goal system

--- Content.Server/Fax/FaxMachineComponent.cs
+++ Content.Server/Fax/FaxMachineComponent.cs
@@ -51,6 +51,13 @@ public sealed class FaxMachineComponent : Component
     public bool ReceiveNukeCodes { get; set; } = false;
 
     /// <summary>
+    /// Should that fax receive station goal info
+    /// </summary>
+    [ViewVariables(VVAccess.ReadWrite)]
+    [DataField("receiveStationGoal")]
+    public bool ReceiveStationGoal { get; set; } = false;
+
+    /// <summary>
     /// Is fax was emaaged
     /// </summary>
     [ViewVariables(VVAccess.ReadWrite)]
