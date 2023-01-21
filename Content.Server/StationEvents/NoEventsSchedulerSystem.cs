c9b2b6da19423654b50b1a14f976709c7425cdc4
HitPanda <104197232+EnefFlow@users.noreply.github.com>
2023-01-22 00:40:22 +0300

    NoEvents Game Rule (#153)

---
+++ Content.Server/StationEvents/NoEventsSchedulerSystem.cs
@@ -0,0 +1,22 @@
+﻿using Content.Server.GameTicking.Rules;
+using JetBrains.Annotations;
+
+namespace Content.Server.StationEvents
+{
+    /// <summary>
+    ///     Game Rule without events.
+    /// </summary>
+    [UsedImplicitly]
+    public sealed class NoEventsSchedulerSystem : GameRuleSystem
+    {
+        public override string Prototype => "NoEventsScheduler";
+
+        public override void Started() { }
+
+        public override void Ended() { }
+
+        public override void Update(float frameTime) { }
+
+
+    }
+}
